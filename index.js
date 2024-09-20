67ee9ea7b752c9327221016c7ba8b9aa53db50f8
starr4ever@TransEdenV4 <starflowerr777@gmail.com>
2024-09-20 13:14:24 +0200

    remove some unnecessary abstractions

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-
-// I put methods I plan to use for this website into static classes
-// to make code cleaner and the whole thing easier to comprehend as
-// what it is doing at any given time.
+/*
+ * DECLARATIONS
+ */
 let contentAdderInstance;
+const kbityArray = [
+  "assets/images/KBITY_TRANSEDEN_TEAL_HEHE_.png",
+  "assets/images/kbity-antinihility-black.png",
+  "assets/images/kbity-chroniclers-gray.png",
+  "assets/images/kbity-dante-green.png",
+  "assets/images/kbity-abyssal-lavender.png",
+  "assets/images/kbity-magical-pink.png",
+  "assets/images/kbity-mainframe-cerulean.png",
+  "assets/images/kbity-matrix-green.png",
+  "assets/images/kbity-order-teal.png",
+  "assets/images/kbity-otherworldly-blue.png",
+  "assets/images/kbity-pandemonium-red.png",
+  "assets/images/kbity-relentless-magenta.png",
+  "assets/images/kbity-system-blue.png",
+  "assets/images/kbity-the-fifth-crimson.png",
+]
 
+/*
+ * FUNCTIONS & HELPER CLASSES
+ */
 // This is so pointless but okay girl. have fun i guess.
 class ContentAddersSingleton {
   constructor() {
@@ -27,11 +45,11 @@ class ContentAddersSingleton {
               src="assets/images/neocities.png"
               onerror="this.src='../assets/images/neocities.png';"
               alt="Neocities logo with a caption &quot;Powered by Neocities&quot;">
-            <p>02.2024 - THIS_VERY_FEMTOSECOND, by ME</p>
+            <p>02.2024 - 09.2024, by LVSA</p>
           </footer>
         </div>
         <div class="post-footer">
-          <a href="https://github.com/PerfectMach1ne/">#."GitHub repo"_</a>
+          <a href="https://github.com/PerfectMach1ne/">#."Me GitHub"_</a>
         </div>
         <div class="post-footer">
           <p>#.EOF_</p>
@@ -54,36 +72,19 @@ class SelectorClassAdders {
   }
 }
 
-class KbityRandomizer {
-  static #kbityArray = [
-    "assets/images/KBITY_TRANSEDEN_TEAL_HEHE_.png",
-    "assets/images/kbity-antinihility-black.png",
-    "assets/images/kbity-chroniclers-gray.png",
-    "assets/images/kbity-dante-green.png",
-    "assets/images/kbity-abyssal-lavender.png",
-    "assets/images/kbity-magical-pink.png",
-    "assets/images/kbity-mainframe-cerulean.png",
-    "assets/images/kbity-matrix-green.png",
-    "assets/images/kbity-order-teal.png",
-    "assets/images/kbity-otherworldly-blue.png",
-    "assets/images/kbity-pandemonium-red.png",
-    "assets/images/kbity-relentless-magenta.png",
-    "assets/images/kbity-system-blue.png",
-    "assets/images/kbity-the-fifth-crimson.png",
-  ]
-
-  static kbityRandomizer() {
-    function getRandomInt(min, max) {
-      const minCeiled = Math.ceil(min);
-      const maxFloored = Math.floor(max);
-      return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-    }
-
-    return this.#kbityArray[getRandomInt(0, this.#kbityArray.length - 1)];
+function kbityRandomizer() {
+  function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
   }
+
+  return kbityArray[getRandomInt(0, kbityArray.length - 1)];
 }
 
-// All the ordinary utility function calls are below.
+/*
+ * FUNCTIONALITY
+ */
 // ContentAdders.footerPaster();
 contentAdderInstance = new ContentAddersSingleton();
 contentAdderInstance.getInstance().footerPaster();
@@ -94,8 +95,8 @@ SelectorClassAdders.saveMarginsPaddings("dl");
 SelectorClassAdders.saveMarginsPaddings("dt");
 SelectorClassAdders.saveMarginsPaddings("dd");
 
-document.getElementById("kbity-left").src=KbityRandomizer.kbityRandomizer();
-document.getElementById("kbity-right").src=KbityRandomizer.kbityRandomizer();
+document.getElementById("kbity-left").src = kbityRandomizer();
+document.getElementById("kbity-right").src = kbityRandomizer();
 
 const button_ids = ["cv-puter", "blog-puter", "nw4", "omega-psi", "co-re"]
 let buttons = Array();
@@ -103,7 +104,7 @@ button_ids.forEach((id_str) => {
   buttons.push(document.getElementById(id_str));
 });
 buttons.forEach((butt) => {
-  butt.addEventListener("click", (e) => {
+  butt.addEventListener("click", () => {
     window.location = window.origin + "/views-pages/" + butt.id + ".html";
   })
 })
